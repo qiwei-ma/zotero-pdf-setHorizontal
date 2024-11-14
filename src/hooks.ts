62aeadc14f840925370e2d8a0837183bcbd97251
Qiwei-Ma <maqiwei303@foxmail.com>
2024-11-14 22:23:00 +0800

    first commit

---
+++ src/hooks.ts
@@ -0,0 +1,99 @@
+
+import { config } from "../package.json";
+import { getString, initLocale } from "./utils/locale";
+import { registerPrefsScripts } from "./modules/preferenceScript";
+import { createZToolkit } from "./utils/ztoolkit";
+import { PdfViewDefaults, } from "./modules/pdfviewdefaults";
+
+async function onStartup() {
+  await Promise.all([
+    Zotero.initializationPromise,
+    Zotero.unlockPromise,
+    Zotero.uiReadyPromise,
+  ]);
+
+  initLocale();
+
+  await Promise.all(
+    Zotero.getMainWindows().map((win) => onMainWindowLoad(win)),
+  );
+}
+
+async function onMainWindowLoad(win: Window): Promise<void> {
+  
+  // 
+  // PdfViewDefaults.registerSetter();
+  PdfViewDefaults.registerSetter(win);
+  // Create ztoolkit for every window
+  addon.data.ztoolkit = createZToolkit();
+
+  // @ts-ignore This is a moz feature
+  win.MozXULElement.insertFTLIfNeeded(`${config.addonRef}-mainWindow.ftl`);
+
+  const popupWin = new ztoolkit.ProgressWindow(config.addonName, {
+    closeOnClick: true,
+    closeTime: -1,
+  })
+    .createLine({
+      text: getString("startup-begin"),
+      type: "default",
+      progress: 0,
+    })
+    .show();
+
+  await Zotero.Promise.delay(1000);
+  popupWin.changeLine({
+    progress: 30,
+    text: `[30%] ${getString("startup-begin")}`,
+  });
+
+  await Zotero.Promise.delay(1000);
+  popupWin.changeLine({
+    progress: 100,
+    text: `[100%] ${getString("startup-finish")}`,
+  });
+  popupWin.startCloseTimer(5000);
+}
+
+async function onMainWindowUnload(win: Window): Promise<void> {
+  ztoolkit.unregisterAll();
+  addon.data.dialog?.window?.close();
+}
+
+function onShutdown(): void {
+  ztoolkit.unregisterAll();
+  addon.data.dialog?.window?.close();
+  // Remove addon object
+  addon.data.alive = false;
+  delete Zotero[config.addonInstance];
+}
+
+
+/**
+ * This function is just an example of dispatcher for Preference UI events.
+ * Any operations should be placed in a function to keep this funcion clear.
+ * @param type event type
+ * @param data event data
+ */
+async function onPrefsEvent(type: string, data: { [key: string]: any }) {
+  switch (type) {
+    case "load":
+      registerPrefsScripts(data.window);
+      break;
+    default:
+      return;
+  }
+}
+
+
+// Add your hooks here. For element click, etc.
+// Keep in mind hooks only do dispatch. Don't add code that does real jobs in hooks.
+// Otherwise the code would be hard to read and maintain.
+
+export default {
+  onStartup,
+  onShutdown,
+  onMainWindowLoad,
+  onMainWindowUnload,
+  onPrefsEvent,
+}
